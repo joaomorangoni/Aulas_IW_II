98e43dc7a236d41a273d56ba512833346eb69449
joaomorangoni <joaopedromorangoni@gmail.com>
2024-03-07 09:48:21 -0300

    aaaa

--- ExercicioLampada/js/script.js
+++ ExercicioLampada/js/script.js
@@ -1,8 +1,8 @@
 const ligar = document.getElementById('ligar')
-const desligar = document.getElementById('desligar')
 const lampada = document.getElementById('lamp')
 const rest = document.getElementById('restaurar')
 var texto = document.getElementById('estado')
+var ligado = false
 
 function Quebrada(){
     return lampada.src.indexOf('quebrada') > -1
@@ -11,8 +11,16 @@ function Quebrada(){
 
 function lampLigada(){
     if (!Quebrada()){
-        lampada.src = "img/ligada.jpg"
-        texto.innerHTML = 'LIGADA';
+        if (!ligado){
+            lampada.src = "img/ligada.jpg"
+            ligado = true
+            texto.innerHTML = 'LIGADA';
+        }
+        else if (ligado){
+            lampada.src = "img/desligada.jpg"
+            ligado = false
+            texto.innerHTML = 'DESLIGADA';
+        }
     }
 }
 
@@ -34,7 +42,6 @@ function lampRest(){
 }
 
 ligar.addEventListener('click',lampLigada)
-desligar.addEventListener('click',lampDesligada)
 
 lampada.addEventListener('dblclick',lampQuebrar)
 rest.addEventListener('click',lampRest)
